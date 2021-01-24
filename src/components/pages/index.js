3f55bbe9090998b98f0c8a6bf72e080700b17776
SyrgakRavshanbekUulu <75747709+SyrgakRavshanbekUulu@users.noreply.github.com>
2021-01-24 16:21:51 +0600

    'first-commit'

---
+++ src/components/pages/index.js
@@ -0,0 +1,10 @@
+import CharacterPage from './charactersPage';
+import BooksPage from './booksPage';
+import HousesPage from './housesPage';
+import BooksItem from './booksItem';
+export {
+    CharacterPage,
+    BooksPage,
+    HousesPage,
+    BooksItem
+}
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-spin-znknc62m6u">
+            <div className="ldio-w55pl33iqfd">
+                 <div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
+            </div>
+        </div>
+    )
+
+}
+export default Spinner;
+
